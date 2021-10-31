00c8f5cdcb4e7d5a196f7f7aa1733729ef8ff060
Yanxiu Wuwang <uofvo@student.kit.edu>
2021-10-31 12:51:17 +0100

    setup login route(need this in all cases), ui for login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '@/views/Login.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -14,11 +15,17 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
+
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
---
+++ src/views/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <h2>Login</h2>
+    <div v-if="error">
+
+    </div>
+    <form method="post">
+      <div class="form-group mb-3">
+        <label for="username" class="form-label">Username</label>
+        <input autofocus id="username" class="form-control" type="text" name="username" placeholder="Username">
+      </div>
+      <div class="form-group mb-3">
+        <label for="homeserver" class="form-label">Homeserver</label>
+        <input id="homeserver" class="form-control" type="text" name="homeserver" placeholder="Homeserver">
+      </div>
+      <div class="form-group mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input id="password" class="form-control" type="password" name="password" placeholder="Password">
+      </div>
+      <input class="btn btn-primary" type="submit" value="Login">
+    </form>
+
+    Don't have an account? <a href="#">Register here.</a>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Login'
+})
+</script>
+<style scoped>
+
+</style>
