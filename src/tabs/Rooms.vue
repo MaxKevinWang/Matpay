5b3e6ca3c301d88f048c31417f8b178725b2f774
Yanxiu Wuwang <uofvo@student.kit.edu>
2021-11-03 13:58:44 +0100

    implement ui for room list

--- src/tabs/Rooms.vue
+++ src/tabs/Rooms.vue
@@ -1,14 +1,63 @@
 <template>
-<div>
-  Rooms here.
-</div>
+  <div class="container">
+    <h2>Rooms</h2>
+    <div class="alert alert-danger" role="alert" v-if="!room_exists">
+      No rooms joined.
+    </div>
+    <table class="table" v-if="room_exists">
+      <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Name</th>
+        <th scope="col">Member Count</th>
+        <th scope="col">User Type</th>
+        <th scope="col">Actions</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="room in rooms" :key="room.id">
+        <th scope="row">{{ room.id }}</th>
+        <td>{{ room.name }}</td>
+        <td>{{ room.member_count }}</td>
+        <td>{{ room.user_type }}</td>
+        <td>
+          <button class="btn btn-primary" @click="joinRoom(room.id)">Details</button>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+interface RoomInterface {
+  id: number
+  name: string
+  member_count: number
+  user_type: string
+}
+
 export default defineComponent({
-  name: 'Rooms'
+  name: 'Rooms',
+  data () {
+    return {
+      rooms: [
+        {
+          id: 1234,
+          name: 'xxxx',
+          member_count: 0,
+          user_type: 'admin'
+        }
+      ] as RoomInterface[]
+    }
+  },
+  computed: {
+    room_exists (): boolean {
+      return !!this.rooms && this.rooms.length > 0
+    }
+  }
 })
 
 </script>
