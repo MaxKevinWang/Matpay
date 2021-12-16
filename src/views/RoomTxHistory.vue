12a0cf26607b79d417f24bf3f0d87f4466c441b6
André Ackermann <uospa@student.kit.edu>
2021-12-16 18:51:10 +0100

    Added first elements for the ui of the balance

--- src/views/RoomTxHistory.vue
+++ src/views/RoomTxHistory.vue
@@ -1,6 +1,25 @@
 <template>
-<div>
-</div>
+  <div class="LeftHxView">
+    <h3>{{ room_name }}</h3>
+    <div class="row">
+      <div class="col-4">
+        <div class="list-group" id="list-tab" role="tablist">
+          <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">Home</a>
+          <a class="list-group-item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">Profile</a>
+          <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Messages</a>
+          <a class="list-group-item list-group-item-action" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Settings</a>
+        </div>
+      </div>
+      <div class="col-8">
+        <div class="tab-content" id="nav-tabContent">
+          <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">Erscheint das dann!</div>
+          <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">Das?</div>
+          <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
+          <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -14,6 +33,16 @@ export default defineComponent({
     }
   },
   computed: {
+    ...mapGetters('rooms', [
+      'get_member_state_events_for_room',
+      'get_room_name'
+    ]),
+    room_id (): string {
+      return this.$route.params.room_id as string
+    },
+    room_name (): string {
+      return this.get_room_name(this.room_id)
+    }
   },
   components: {
   },
@@ -22,4 +51,10 @@ export default defineComponent({
 })
 </script>
 <style scoped>
+.LeftHxView {
+  left: 0;
+  top: 0;
+  padding: 20px;
+  z-index: 7
+}
 </style>
