cf57f3f534f003bcb7259a0cb067311a0d18ef74
Yanxiu Wuwang <uofvo@student.kit.edu>
2021-12-17 17:14:07 +0100

    initialize empty dialogs

---
+++ src/dialogs/CreateRoomDialog.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="modal fade" id="<...>-modal" tabindex="-1" aria-labelledby="<...>-label" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="<...>-label">Some title here</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <!-- Content of dialog here -->
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">OK</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+/* eslint-disable no-unused-expressions */
+import { defineComponent, PropType } from 'vue'
+import { mapActions, mapGetters } from 'vuex'
+import { Modal, Popover } from 'bootstrap'
+
+export default defineComponent({
+  name: 'CreateRoomDialog',
+  props: {
+    room_id: {
+      type: String as PropType<string>
+    }
+  },
+  data () {
+    return {
+      modal_control: null as Modal | null,
+      is_shown: false as boolean
+    }
+  },
+  computed: {
+  },
+  components: {
+  },
+  methods: {
+    show () {
+      this.modal_control?.show()
+      this.is_shown = true
+    },
+    hide () {
+      this.modal_control?.hide()
+      this.is_shown = false
+    }
+  },
+  mounted () {
+    this.modal_control = new Modal(document.getElementById('<...>-modal') as HTMLElement, {
+      backdrop: false
+    })
+  }
+})
+</script>
+<style scoped>
+
+</style>
