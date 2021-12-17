3ea1d284cfcc864294040a4cd9dca63edbbec57d
André Ackermann <uospa@student.kit.edu>
2021-12-17 20:33:28 +0100

    Added some stuff to RoomTxHistory

--- src/views/RoomTxHistory.vue
+++ src/views/RoomTxHistory.vue
@@ -1,21 +1,11 @@
 <template>
   <div class="LeftHxView">
     <h3>{{ room_name }}</h3>
-    <div class="row">
+    <h3>Your balance: </h3>
+    <div class="row" v-if="grouped_transactions.size >= 1">
       <div class="col-4">
         <div class="list-group" id="list-tab" role="tablist">
-          <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">Home</a>
-          <a class="list-group-item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">Profile</a>
-          <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Messages</a>
-          <a class="list-group-item list-group-item-action" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Settings</a>
-        </div>
-      </div>
-      <div class="col-8">
-        <div class="tab-content" id="nav-tabContent">
-          <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">Erscheint das dann!</div>
-          <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">Das?</div>
-          <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
-          <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>
+          <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home" v-for="grouped_transaction in grouped_transactions" :key="grouped_transaction.timestamp">{{ time }} Description: {{ from }} paid {{ amount }}</a>
         </div>
       </div>
     </div>
@@ -23,6 +13,7 @@
 </template>
 
 <script lang="ts">
+import { GroupedTransaction } from '@/models/transaction.model'
 import { defineComponent } from 'vue'
 import { mapActions, mapGetters } from 'vuex'
 
@@ -30,23 +21,41 @@ export default defineComponent({
   name: 'RoomTxHistory',
   data () {
     return {
+      room_name: '' as string,
+      from: '' as string,
+      amount: 0 as number,
+      grouped_transactions: [] as Array<GroupedTransaction>,
+      time: '' as string
     }
   },
   computed: {
     ...mapGetters('rooms', [
-      'get_member_state_events_for_room',
       'get_room_name'
     ]),
     room_id (): string {
       return this.$route.params.room_id as string
     },
-    room_name (): string {
-      return this.get_room_name(this.room_id)
-    }
+    ...mapGetters('tx', [
+      'get_grouped_transactions_for_room'
+    ])
   },
   components: {
   },
   methods: {
+    set_up () {
+      this.room_name = this.get_room_name(this.room_id)
+      this.grouped_transactions = this.get_grouped_transactions_for_room(this.room_id)
+    },
+    make_tx_list (grouped_transaction: GroupedTransaction) {
+      this.from = grouped_transaction.from.displayname
+      this.time = grouped_transaction.timestamp.getDay + ' ' + this.grouped_transactions[0].timestamp.getMonth
+    },
+    calc_amount (grouped_transaction: GroupedTransaction) {
+      grouped_transaction.txs.forEach(txs => {
+        this.amount += txs.amount
+      })
+      return this.amount
+    }
   }
 })
 </script>
