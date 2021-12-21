909c3bdafb094aad274a5d568c4332e151e045e5
Xuyang Hou <ujfdh@student.kit.edu>
2021-12-21 16:34:56 +0100

    fix problems of tx-box

--- src/components/TxMessageBox.vue
+++ src/components/TxMessageBox.vue
@@ -1,12 +1,40 @@
 <template>
-  <div class="card">
+  <div class="card" v-if="this.reference.type === 'approved'"
+       :style="['background-color: rgba(105, 105, 105,.5)']">
     <div class="card-body">
-      <h5 class="card-title"></h5>
-      <p class="card-text"></p>
-      <p>{{this.reference.timestamp.toLocaleDateString()}}</p>
-      <p>{{this.reference.grouped_tx.description}}</p>
-      <p>{{this.reference.grouped_tx.from.displayname + " paid " + this.calc_amount(this.reference.grouped_tx)+"$"}}</p>
-      <a href="#" class="btn btn-primary">details</a>
+      <div class="row">
+        <div class="col">
+          <p>{{this.reference.timestamp.toLocaleDateString()}}</p>
+        </div>
+        <div class="col">
+          <p>{{this.reference.grouped_tx.description}}</p>
+        </div>
+        <div class="col">
+          <p>{{this.reference.grouped_tx.from.displayname + " paid " + this.calc_amount(this.reference.grouped_tx)+"$"}}</p>
+        </div>
+        <div class="col">
+         <a href="#" class="btn btn-primary">details</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="card" v-if="this.reference.type === 'pending'"
+       :style="['background-color: rgba(255, 193, 193,.5)']">
+    <div class="card-body">
+      <div class="row">
+        <div class="col">
+          <p>{{this.reference.timestamp.toLocaleDateString()}}</p>
+        </div>
+        <div class="col">
+          <p>{{this.reference.approval.description}}</p>
+        </div>
+        <div class="col">
+          <p>{{this.reference.approval.from.displayname + " paid " + this.calc_amount2(this.reference.approval)+"$"}}</p>
+        </div>
+        <div class="col">
+          <a href="#" class="btn btn-primary">details</a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -54,9 +82,16 @@ export default defineComponent({
   methods: {
     calc_amount (tx: GroupedTransaction) : number {
       let amount = 0
-      tx.txs.forEach(txs => {
-        amount += txs.amount
-      })
+      for (const simple_tx of tx.txs) {
+        amount += simple_tx.amount
+      }
+      return amount
+    },
+    calc_amount2 (tx: PendingApproval) : number {
+      let amount = 0
+      for (const simple_tx of tx.txs) {
+        amount += simple_tx.amount
+      }
       return amount
     }
   }
@@ -66,7 +101,6 @@ export default defineComponent({
 
 .card {
   position: relative;
-  background: darkgrey;
   margin-bottom: 30px;
 }
 
