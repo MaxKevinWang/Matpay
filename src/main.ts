7c1ff739eb49224a9ba8a745dc71b4a37fa8bf25
Zhi Wang <ubwsx@student.kit.edu>
2021-12-23 14:52:33 +0100

    Add a function not finished

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import axios from 'axios'
-import { GroupedTransaction, PendingApproval } from '@/models/transaction.model'
+import { GroupedTransaction, PendingApproval, SimpleTransaction } from '@/models/transaction.model'
+import { TxID } from '@/models/id.model'
 
 // register axios interceptor
 axios.interceptors.request.use(function (config) {
@@ -23,5 +24,10 @@ const app = createApp(App)
 app.use(store).use(router)
 app.config.globalProperties.sum_amount = (item: GroupedTransaction | PendingApproval) : number => {
   return item.txs.reduce((sum, tx) => sum + tx.amount, 0)
+}
+app.config.globalProperties.split_percentage = (item: GroupedTransaction | PendingApproval) : Record<TxID, number> => {
+  for (const simple_tx of item.txs) {
+    return <simple_tx.tx_id, simple_tx.amount / app.config.globalProperties.sum_amount(item)>
+  }
 }
 app.mount('#app')
