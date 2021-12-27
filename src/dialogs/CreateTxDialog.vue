6edb1404e6e23eb34b408875bb6b336e26d6b80f
André Ackermann <uospa@student.kit.edu>
2021-12-27 15:53:40 +0100

    changed is_number()

--- src/dialogs/CreateTxDialog.vue
+++ src/dialogs/CreateTxDialog.vue
@@ -9,20 +9,15 @@
         <div class="modal-body d-grid gap-2">
           <div class="input-group">
             <span id="basic-addon1" class="input-group-text">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-receipt" viewBox="0 0 16 16">
-                <path d="M1.92.506a.5.5 0 0 1 .434.14L3 1.293l.646-.647a.5.5 0 0 1 .708 0L5 1.293l.646-.647a.5.5 0 0 1 .708 0L7 1.293l.646-.647a.5.5 0 0 1 .708 0L9 1.293l.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .801.13l.5 1A.5.5 0 0 1 15 2v12a.5.5 0 0 1-.053.224l-.5 1a.5.5 0 0 1-.8.13L13 14.707l-.646.647a.5.5 0 0 1-.708 0L11 14.707l-.646.647a.5.5 0 0 1-.708 0L9 14.707l-.646.647a.5.5 0 0 1-.708 0L7 14.707l-.646.647a.5.5 0 0 1-.708 0L5 14.707l-.646.647a.5.5 0 0 1-.708 0L3 14.707l-.646.647a.5.5 0 0 1-.801-.13l-.5-1A.5.5 0 0 1 1 14V2a.5.5 0 0 1 .053-.224l.5-1a.5.5 0 0 1 .367-.27zm.217 1.338L2 2.118v11.764l.137.274.51-.51a.5.5 0 0 1 .707 0l.646.647.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.509.509.137-.274V2.118l-.137-.274-.51.51a.5.5 0 0 1-.707 0L12 1.707l-.646.647a.5.5 0 0 1-.708 0L10 1.707l-.646.647a.5.5 0 0 1-.708 0L8 1.707l-.646.647a.5.5 0 0 1-.708 0L6 1.707l-.646.647a.5.5 0 0 1-.708 0L4 1.707l-.646.647a.5.5 0 0 1-.708 0l-.509-.51z"/>
-                <path d="M3 4.5a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm8-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5z"/>
-              </svg>
+              <i class="bi bi-receipt"></i>
             </span>
             <input v-model="description" type="text" class="form-control" placeholder="Description" aria-label="Description" aria-describedby="basic-addon1" data-bs-toggle="popover" id="input-description">
           </div>
           <div class="input-group">
-            <span id="basic-addon1" class="input-group-text">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-euro" viewBox="0 0 16 16">
-                <path d="M4 9.42h1.063C5.4 12.323 7.317 14 10.34 14c.622 0 1.167-.068 1.659-.185v-1.3c-.484.119-1.045.17-1.659.17-2.1 0-3.455-1.198-3.775-3.264h4.017v-.928H6.497v-.936c0-.11 0-.219.008-.329h4.078v-.927H6.618c.388-1.898 1.719-2.985 3.723-2.985.614 0 1.175.05 1.659.177V2.194A6.617 6.617 0 0 0 10.341 2c-2.928 0-4.82 1.569-5.244 4.3H4v.928h1.01v1.265H4v.928z"/>
-              </svg>
+            <span id="basic-addon2" class="input-group-text">
+              <i class="bi bi-currency-euro"></i>
             </span>
-            <input v-model="amount" type="text" class="form-control" placeholder="Amount" aria-label="Amount" aria-describedby="basic-addon1" data-bs-toggle="popover" id="input-amount">
+            <input v-model="amount_input" type="text" class="form-control" placeholder="Amount" aria-label="Amount" aria-describedby="basic-addon1" data-bs-toggle="popover" id="input-amount">
           </div>
           <button class="btn btn-primary" @click="on_split_configuration_clicked()">Split Configuration</button>
         </div>
@@ -51,7 +46,7 @@ export default defineComponent({
       type: String as PropType<string>
     },
     room_name: {
-      type: String as PropType<string>
+      type: Number as PropType<number>
     },
     users_info: {
       type: Object as PropType<Array<RoomUserInfo>>
@@ -62,7 +57,8 @@ export default defineComponent({
       modal_control: null as Modal | null,
       is_shown: false as boolean,
       description: '' as string,
-      amount: '' as string
+      amount_input: '' as string,
+      amount: 0 as number
     }
   },
   computed: {
@@ -95,7 +91,7 @@ export default defineComponent({
     popover_no_number (number : boolean) {
       if (!number) {
         const popover = new Popover('#input-amount', {
-          content: 'Amount has to be a number',
+          content: 'Amount has to be a positive number',
           container: 'body'
         })
         popover.show()
@@ -104,7 +100,8 @@ export default defineComponent({
     },
     on_confirm () {
       if (this.description.length >= 1 && this.is_number()) {
-        this.amount = ''
+        this.amount = parseFloat(this.amount_input)
+        this.amount_input = ''
         this.description = ''
         this.hide()
       } else {
@@ -113,19 +110,20 @@ export default defineComponent({
       }
     },
     is_number () : boolean {
-      if (this.amount.includes(',')) {
-        const split_amount = this.amount.split(',')
-        if (split_amount.length === 2 && split_amount[1].length <= 2 && split_amount[0].match(/^\d+$/) !== null && split_amount[1].match(/^\d+$/) !== null) {
-          return true
-        } else {
-          return false
-        }
+      let check_amount = this.amount_input
+      if (check_amount.includes(',')) {
+        check_amount = check_amount.replace(',', '.')
+      }
+      /*
+       if it´s not a number it returns false
+      */
+      if (isNaN(Number(check_amount))) {
+        return false
       } else {
-        if (this.amount.match(/^\d+$/) !== null) {
-          return true
-        } else {
+        if (check_amount.charAt(0) === '-') {
           return false
         }
+        return true
       }
     }
   },
