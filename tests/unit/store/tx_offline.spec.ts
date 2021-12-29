3c4449c15370bf3adbf2fd968b87998535198ab6
Yanxiu Wuwang <uofvo@student.kit.edu>
2021-12-29 16:30:30 +0100

    write basic tests for create and modify

--- tests/unit/store/tx_offline.spec.ts
+++ tests/unit/store/tx_offline.spec.ts
@@ -1,7 +1,7 @@
 import store from '@/store/tx'
 import { MatrixEventID, MatrixRoomID, MatrixUserID } from '@/models/id.model'
 import { GroupedTransaction, PendingApproval, TxGraph } from '@/models/transaction.model'
-import { TxCreateEvent } from '@/interface/tx_event.interface'
+import { TxCreateEvent, TxModifyEvent } from '@/interface/tx_event.interface'
 import { test_account1, test_account2 } from '../../test_utils'
 import { uuidgen } from '@/utils/utils'
 import { room_01_user_info, user_1, user_2 } from '../mocks/mocked_user'
@@ -30,12 +30,12 @@ describe('Test transaction Vuex store offline', () => {
               graph: {}
             },
             is_graph_dirty: false,
-            rejected: {},
+            rejected: {}
           }
         }
       }
       const rootGetters = {
-        'user/get_user_info_for_room': (r: MatrixRoomID) => {
+        'user/get_users_info_for_room': (r: MatrixRoomID) => {
           if (r === room_id) {
             return room_01_user_info
           }
@@ -51,13 +51,19 @@ describe('Test transaction Vuex store offline', () => {
                 graph: {}
               },
               is_graph_dirty: false,
-              rejected: {},
+              rejected: {}
             }
           }
         }
       })
       describe('Test parse create event', () => {
         it('Test invalid UUID', async () => {
+          const getters = {
+            get_grouped_transactions_for_room: store.getters.get_grouped_transactions_for_room(state, null, null, null),
+            get_pending_approvals_for_room: store.getters.get_pending_approvals_for_room(state, null, null, null),
+            get_existing_group_ids_for_room: store.getters.get_existing_group_ids_for_room(state, null, null, null),
+            get_existing_tx_ids_for_room: store.getters.get_existing_tx_ids_for_room(state, null, null, null)
+          }
           const event: TxCreateEvent = {
             type: 'com.matpay.create',
             sender: user_1.user_id,
@@ -81,7 +87,93 @@ describe('Test transaction Vuex store offline', () => {
             state,
             commit: jest.fn(),
             dispatch: jest.fn(),
-            getters: store.getters,
+            getters: getters,
+            rootGetters: rootGetters
+          }, {
+            room_id: room_id,
+            tx_event: event
+          })).resolves.toEqual(false)
+        })
+        it('Test repeating grouped UUID', async () => {
+          const getters = {
+            get_grouped_transactions_for_room: store.getters.get_grouped_transactions_for_room(state, null, null, null),
+            get_pending_approvals_for_room: store.getters.get_pending_approvals_for_room(state, null, null, null),
+            get_existing_group_ids_for_room: store.getters.get_existing_group_ids_for_room(state, null, null, null),
+            get_existing_tx_ids_for_room: store.getters.get_existing_tx_ids_for_room(state, null, null, null)
+          }
+          const repeated_id = uuidgen()
+          const event: TxCreateEvent = {
+            type: 'com.matpay.create',
+            sender: user_1.user_id,
+            room_id: room_id,
+            origin_server_ts: 60000,
+            event_id: 'e01',
+            content: {
+              from: user_1.user_id,
+              txs: [
+                {
+                  to: user_2.user_id,
+                  amount: 50,
+                  tx_id: uuidgen()
+                }
+              ],
+              group_id: repeated_id,
+              description: 'AAA'
+            }
+          }
+          state.transactions[room_id].pending_approvals.push({
+            event_id: 'BBB',
+            type: 'create',
+            group_id: repeated_id,
+            txs: [],
+            approvals: {},
+            from: user_1,
+            description: 'dfsdgs',
+            timestamp: new Date()
+          })
+          await expect(action({
+            state,
+            commit: jest.fn(),
+            dispatch: jest.fn(),
+            getters: getters,
+            rootGetters: rootGetters
+          }, {
+            room_id: room_id,
+            tx_event: event
+          })).resolves.toEqual(false)
+        })
+      })
+      describe('Test parse modify event', () => {
+        it('Test invalid UUID', async () => {
+          const getters = {
+            get_grouped_transactions_for_room: store.getters.get_grouped_transactions_for_room(state, null, null, null),
+            get_pending_approvals_for_room: store.getters.get_pending_approvals_for_room(state, null, null, null),
+            get_existing_group_ids_for_room: store.getters.get_existing_group_ids_for_room(state, null, null, null),
+            get_existing_tx_ids_for_room: store.getters.get_existing_tx_ids_for_room(state, null, null, null)
+          }
+          const event: TxModifyEvent = {
+            type: 'com.matpay.modify',
+            sender: user_1.user_id,
+            room_id: room_id,
+            origin_server_ts: 60000,
+            event_id: 'e01',
+            content: {
+              txs: [
+                {
+                  to: user_2.user_id,
+                  amount: 50,
+                  tx_id: uuidgen()
+                }
+              ],
+              group_id: 'aaa', // invalid UUID,
+              description: 'AAA'
+            }
+          }
+          await expect(action({
+            state,
+            commit: jest.fn(),
+            dispatch: jest.fn(),
+            getters: getters,
             rootGetters: rootGetters
           }, {
             room_id: 'aaa',
@@ -89,14 +181,11 @@ describe('Test transaction Vuex store offline', () => {
           })).resolves.toEqual(false)
         })
       })
-      describe('Test parse modify event', () => {
-
-      })
       describe('Test parse approve event', () => {
-
+        console.log()
       })
       describe('Test parse settle event', () => {
-
+        console.log()
       })
     })
   })
