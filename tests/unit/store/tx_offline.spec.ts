d2e47a93320fd9a3cfd4361388ba248eb0d667d1
Yanxiu Wuwang <uofvo@student.kit.edu>
2021-12-29 15:58:22 +0100

    prepare tests for event validation

---
+++ tests/unit/store/tx_offline.spec.ts
@@ -0,0 +1,103 @@
+import store from '@/store/tx'
+import { MatrixEventID, MatrixRoomID, MatrixUserID } from '@/models/id.model'
+import { GroupedTransaction, PendingApproval, TxGraph } from '@/models/transaction.model'
+import { TxCreateEvent } from '@/interface/tx_event.interface'
+import { test_account1, test_account2 } from '../../test_utils'
+import { uuidgen } from '@/utils/utils'
+import { room_01_user_info, user_1, user_2 } from '../mocks/mocked_user'
+
+interface State {
+  transactions: Record<MatrixRoomID, {
+    basic: GroupedTransaction[],
+    pending_approvals: PendingApproval[],
+    graph: TxGraph
+    is_graph_dirty: boolean // is the graph updated to the basic
+    rejected: Record<MatrixEventID, Set<MatrixUserID>>,
+  }>
+}
+
+describe('Test transaction Vuex store offline', () => {
+  describe('Test actions', () => {
+    describe('Test action parse single event', () => {
+      const action = store.actions.action_parse_single_tx_event_for_room as (context: any, payload: any) => Promise<boolean>
+      const room_id = 'aaa'
+      let state: State = {
+        transactions: {
+          aaa: {
+            basic: [],
+            pending_approvals: [],
+            graph: {
+              graph: {}
+            },
+            is_graph_dirty: false,
+            rejected: {},
+          }
+        }
+      }
+      const rootGetters = {
+        'user/get_user_info_for_room': (r: MatrixRoomID) => {
+          if (r === room_id) {
+            return room_01_user_info
+          }
+        }
+      }
+      beforeEach(() => {
+        state = { // clear mocks
+          transactions: {
+            aaa: {
+              basic: [],
+              pending_approvals: [],
+              graph: {
+                graph: {}
+              },
+              is_graph_dirty: false,
+              rejected: {},
+            }
+          }
+        }
+      })
+      describe('Test parse create event', () => {
+        it('Test invalid UUID', async () => {
+          const event: TxCreateEvent = {
+            type: 'com.matpay.create',
+            sender: user_1.user_id,
+            room_id: room_id,
+            origin_server_ts: 60000,
+            event_id: 'e01',
+            content: {
+              from: user_1.user_id,
+              txs: [
+                {
+                  to: user_2.user_id,
+                  amount: 50,
+                  tx_id: uuidgen()
+                }
+              ],
+              group_id: 'aaa', // invalid UUID,
+              description: 'AAA'
+            }
+          }
+          await expect(action({
+            state,
+            commit: jest.fn(),
+            dispatch: jest.fn(),
+            getters: store.getters,
+            rootGetters: rootGetters
+          }, {
+            room_id: 'aaa',
+            tx_event: event
+          })).resolves.toEqual(false)
+        })
+      })
+      describe('Test parse modify event', () => {
+
+      })
+      describe('Test parse approve event', () => {
+
+      })
+      describe('Test parse settle event', () => {
+
+      })
+    })
+  })
+})
