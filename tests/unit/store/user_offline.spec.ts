c6cf743da1cda9ea46c2d619c6f02b7e52a8f2d3
André Ackermann <uospa@student.kit.edu>
2022-01-06 16:55:13 +0100

    Started writing tests

--- tests/unit/store/user_offline.spec.ts
+++ tests/unit/store/user_offline.spec.ts
@@ -6,3 +6,17 @@ interface State {
   users_info: Record<MatrixRoomID, Array<RoomUserInfo>>,
   permissions: Record<MatrixRoomID, MatrixRoomPermissionConfiguration>
 }
+
+describe('Test user store', function () {
+  describe('Test store mutation', function () {
+    const room_id : MatrixRoomID = '!ABC:@dsn.kit.edu'
+    let state : State = {
+      users_info: {
+        
+      },
+      permissions: {
+
+      }
+    }
+  })
+})
