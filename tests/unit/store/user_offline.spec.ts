fc7b3e63671edf42b55f83eabc72b31469ef6101
André Ackermann <uospa@student.kit.edu>
2022-01-12 19:02:50 +0100

    Finished Test for user store

--- tests/unit/store/user_offline.spec.ts
+++ tests/unit/store/user_offline.spec.ts
@@ -4,7 +4,7 @@ import { MatrixEventID, MatrixRoomID, MatrixUserID } from '@/models/id.model'
 import { MatrixRoomMemberStateEvent, MatrixRoomPermissionConfiguration } from '@/interface/rooms_event.interface'
 import { room_01_permission, room_01_room_id, room_01_user_info, user_1, user_2 } from '../mocks/mocked_user'
 import axios, { AxiosResponse } from 'axios'
-import { MatrixError } from '@/interface/error.interface'
+import { Dispatch, DispatchOptions } from 'vuex'
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
@@ -15,9 +15,27 @@ interface State {
 }
 
 describe('Test user store', function () {
-  describe('Test store mutation', function () {
-    const room_id = 'ABC'
-    let state : State = {
+  const room_id = 'ABC'
+  let state : State = {
+    users_info: {
+      ABC: []
+    },
+    permissions: {
+      ABC: {
+        ban: 0,
+        events: {},
+        events_default: 0,
+        invite: 0,
+        kick: 0,
+        redact: 0,
+        state_default: 0,
+        users_default: 0,
+        users: {}
+      }
+    }
+  }
+  beforeEach(() => {
+    state = { // clear mocks
       users_info: {
         ABC: []
       },
@@ -35,26 +53,8 @@ describe('Test user store', function () {
         }
       }
     }
-    beforeEach(() => {
-      state = { // clear mocks
-        users_info: {
-          ABC: []
-        },
-        permissions: {
-          ABC: {
-            ban: 0,
-            events: {},
-            events_default: 0,
-            invite: 0,
-            kick: 0,
-            redact: 0,
-            state_default: 0,
-            users_default: 0,
-            users: {}
-          }
-        }
-      }
-    })
+  })
+  describe('Test store mutation', function () {
     it('Test mutation_init_joined_room', function () {
       const mutation = store.mutations.mutation_init_joined_room
       mutation(state, 'ABC')
@@ -76,52 +76,21 @@ describe('Test user store', function () {
       })
       expect(state.permissions[room_id]).toEqual(room_01_permission)
     })
+    it('Test mutation_reset_state', function () {
+      const mutation = store.mutations.mutation_reset_state
+      state.users_info[room_id] = room_01_user_info
+      state.permissions[room_id] = room_01_permission
+      mutation(state)
+      expect(state.users_info).toEqual({})
+      expect(state.permissions).toEqual({})
+    })
   })
   describe('Test store actions', function () {
-    const room_id = 'ABC'
-    let state: State = {
-      users_info: {
-        ABC: []
-      },
-      permissions: {
-        ABC: {
-          ban: 0,
-          events: {},
-          events_default: 0,
-          invite: 0,
-          kick: 0,
-          redact: 0,
-          state_default: 0,
-          users_default: 0,
-          users: {}
-        }
-      }
-    }
     const action_parse_member = store.actions.action_parse_member_events_for_room as (context: any, payload: any) => Promise<Array<RoomUserInfo>>
     const rootGetters = {
       'auth/homeserver': '!ghjfghkdk:dsn.scc.kit.edu',
       'auth/user_id': '@test-1:dsn.tm.kit.edu'
     }
-    beforeEach(() => {
-      state = { // clear mocks
-        users_info: {
-          ABC: []
-        },
-        permissions: {
-          ABC: {
-            ban: 0,
-            events: {},
-            events_default: 0,
-            invite: 0,
-            kick: 0,
-            redact: 0,
-            state_default: 0,
-            users_default: 0,
-            users: {}
-          }
-        }
-      }
-    })
     describe('Test action_parse_member_events_for_room', function () {
       it('Test no user_name collision', async () => {
         const member_events: MatrixRoomMemberStateEvent[] = [{
@@ -140,7 +109,7 @@ describe('Test user store', function () {
           type: 'm.room.member',
           content: {
             avatar_url: '',
-            displayname: 'DSN Test Account No 2',
+            displayname: '',
             membership: 'join'
           },
           state_key: '@test-2:dsn.tm.kit.edu',
@@ -157,7 +126,17 @@ describe('Test user store', function () {
           room_id: room_id,
           member_events: member_events,
           permission_event: room_01_permission
-        })).resolves.toEqual([room_01_user_info[0], room_01_user_info[2]])
+        })).resolves.toEqual([room_01_user_info[0], {
+          user: {
+            user_id: user_2.user_id,
+            displayname: ''
+          },
+          avatar_url: '',
+          displayname: '@test-2:dsn.tm.kit.edu',
+          is_self: false,
+          user_type: 'Member',
+          balance: 0
+        }])
       })
       it('Test two users with the same displayname', async () => {
         const member_events: MatrixRoomMemberStateEvent[] = [{
@@ -230,6 +209,7 @@ describe('Test user store', function () {
     })
   })
   describe('Test action_change_user_membership_on_room', function () {
+    const action_change_memb = store.actions.action_change_user_membership_on_room as (context: any, payload: any) => Promise<boolean>
     it('Test 400 response', async () => {
       const response = {
         status: 400,
@@ -238,7 +218,6 @@ describe('Test user store', function () {
           error: 'An unknown error occurred'
         }
       }
-      const action_change_memb = store.actions.action_change_user_membership_on_room as (context: any, payload: any) => Promise<boolean>
       mockedAxios.post.mockResolvedValue(response)
       await expect(() => action_change_memb({
         dispatch: jest.fn(),
@@ -249,6 +228,37 @@ describe('Test user store', function () {
         action: 'invite'
       })).rejects.toThrow(response.data.error)
     })
-    it('Test 200 response')
+    it('Test 200 response', async () => {
+      const response = {
+        status: 200,
+        data: ''
+      }
+      let dispatch_called = false
+      const dispatch = () => {
+        dispatch_called = true
+      }
+      mockedAxios.post.mockResolvedValue(response)
+      await action_change_memb({
+        dispatch,
+        rootGetters: jest.fn()
+      }, {
+        room_id: room_01_room_id,
+        user_id: user_1.user_id,
+        action: 'invite'
+      })
+      expect(dispatch_called).toEqual(true)
+    })
+  })
+  describe('Test store getters', function () {
+    it('Test get_users_info_for_room', function () {
+      const getter = store.getters.get_users_info_for_room(state, null, null, null)
+      state.users_info[room_id] = room_01_user_info
+      expect(getter(room_id)).toEqual(room_01_user_info)
+    })
+    it('Test get_permissions_for_room', function () {
+      const getter = store.getters.get_permissions_for_room(state, null, null, null)
+      state.permissions[room_id] = room_01_permission
+      expect(getter(room_id)).toEqual(room_01_permission)
+    })
   })
 })
