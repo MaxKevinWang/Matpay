7dfaead06e50b1d5969a7cc37d23ec6d7e8cc2ca
Yanxiu Wuwang <uofvo@student.kit.edu>
2022-01-15 16:36:06 +0100

    fix example

--- tests/unit/ui/Rooms.spec.ts
+++ tests/unit/ui/Rooms.spec.ts
@@ -4,14 +4,14 @@ import { config, mount, shallowMount } from '@vue/test-utils'
 import Rooms from '@/tabs/Rooms.vue'
 import { nextTick } from 'vue'
 import { split_percentage, sum_amount, to_currency_display } from '@/main'
-beforeAll(() => {
-  config.global.mocks = {
-    sum_amount: sum_amount,
-    split_percentage: split_percentage,
-    to_currency_display: to_currency_display
-  }
-})
 describe('Test Rooms Tab', () => {
+  beforeAll(() => {
+    config.global.mocks = {
+      sum_amount: sum_amount,
+      split_percentage: split_percentage,
+      to_currency_display: to_currency_display
+    }
+  })
   describe('Test Joined Rooms Tab display correctness (Load User State 1)', () => {
     let store = newStore()
     beforeEach(() => {
