b571c0ecf888afbc1abb39a509a81af1cc1230c6
Yanxiu Wuwang <uofvo@student.kit.edu>
2022-01-19 18:14:45 +0100

    fix minor test error

--- tests/e2e/specs/integration/create_payment_group_1.spec.ts
+++ tests/e2e/specs/integration/create_payment_group_1.spec.ts
@@ -1,4 +1,3 @@
-import { test_account1 } from '../../../test_utils'
 import { user_1 } from '../../../unit/mocks/mocked_user'
 import { uuidgen } from '@/utils/utils'
 
@@ -11,18 +10,19 @@ describe('Test US Create Payment Group 1', function () {
     cy.wait(5000)
     cy.get('#create-dialog-button').click()
       .then(() => {
-        const random_room_id = uuidgen().substring(0, 7)
-        cy.get('#room-name-input').type(random_room_id)
+        const random_room_name = uuidgen().substring(0, 7)
+        cy.get('#room-name-input').type(random_room_name)
         cy.get('#create-room-button').click()
           .then(() => {
+            cy.wait(1000)
             cy.url().should('includes', 'room')
-            cy.contains('h2', random_room_id)
+            cy.contains('h2', random_room_name)
             cy.contains('.about', user_1.displayname)
             cy.contains('.about', 'Admin')
             cy.get('.chat-message').should('not.exist')
             cy.visit('/rooms')
               .then(() => {
-                cy.contains('td', random_room_id)
+                cy.contains('td', random_room_name)
               })
           })
       })
