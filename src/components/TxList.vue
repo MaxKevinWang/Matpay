4b279be799bf105e07a98c7ad8b7e01dbe1c65c1
Zhi Wang <ubwsx@student.kit.edu>
2022-01-19 23:48:14 +0100

    TxList test modified

--- src/components/TxList.vue
+++ src/components/TxList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="list-group" id="list-tab" role="tablist" v-for="tx in tx_list" :key="tx.group_id" data-test="todo">
+  <div class="list-group list-tab" role="tablist" v-for="tx in tx_list" :key="tx.group_id" :data-test="tx.group_id">
     <button type="button" class="list-group-item list-group-item-action btn btn-primary" @click="on_click_event(tx)" id="Txlist_button">{{ tx.timestamp.toLocaleDateString() }} {{ tx.description }}: {{ tx.from.displayname }} paid {{ sum_amount(tx) / 100 }}€</button>
   </div>
 </template>
