f30f1fa8b8bb0aef1be0129128529f083827a628
Yanxiu Wuwang <uofvo@student.kit.edu>
2022-01-29 15:44:44 +0100

    resolve modification

--- tests/unit/ui/Rooms.spec.ts
+++ tests/unit/ui/Rooms.spec.ts
@@ -1,9 +1,9 @@
-import { newStore } from '@/store/index'
-import { Room } from '@/models/room.model'
-import { config, mount, shallowMount } from '@vue/test-utils'
+import { config, flushPromises, mount, shallowMount } from '@vue/test-utils'
 import Rooms from '@/tabs/Rooms.vue'
-import { nextTick } from 'vue'
 import { split_percentage, sum_amount, to_currency_display } from '@/utils/utils'
+import { createStore } from 'vuex'
+import { user_1 } from '../mocks/mocked_user'
+import { MatrixEventID, MatrixRoomID, MatrixUserID } from '@/models/id.model'
 describe('Test Rooms Tab', () => {
   beforeAll(() => {
     config.global.mocks = {
@@ -13,19 +13,63 @@ describe('Test Rooms Tab', () => {
     }
   })
   describe('Test Joined Rooms Tab display correctness (Load User State 1)', () => {
-    let store = newStore()
-    beforeEach(() => {
-      store = newStore()
+    const store = createStore({
+      modules: {
+        auth: {
+          namespaced: true,
+          state () {
+            return {
+              user_id: user_1.user_id,
+              homeserver: 'https://tchncs.de',
+              access_token: 'token',
+              device_id: undefined
+            }
+          },
+          getters: {
+            user_id: (state) => () => state.user_id
+          }
+        },
+        rooms: {
+          namespaced: true,
+          state () {
+            return {
+              joined_rooms: [],
+              invited_rooms: []
+            }
+          },
+          getters: {
+            get_room_table_rows: () => () => [{
+              room_id: 'aaa',
+              room_id_display: 'aaa',
+              name: 'Room1',
+              member_count: 1,
+              user_type: 'admin'
+            }],
+            get_invited_rooms: (state) => () => state.invited_rooms
+          },
+          actions: {
+            action_create_room: jest.fn(),
+            action_accept_invitation_for_room: jest.fn(),
+            action_reject_invitation_for_room: jest.fn()
+          }
+        },
+        sync: {
+          namespaced: true,
+          state () {
+            return {
+              init_state_complete: true
+            }
+          },
+          getters: {
+            is_initial_sync_complete: (state) => () => state.init_state_complete
+          },
+          actions: {
+            action_sync_initial_state: jest.fn()
+          }
+        }
+      }
     })
     it('Test one room', async () => {
-      store.state.rooms.joined_rooms = [
-        {
-          room_id: 'aaa',
-          name: 'Room1',
-          state_events: []
-        }
-      ]
-      // store.dispatch = jest.fn()
       let redirected = false
       const wrapper = shallowMount(Rooms, {
         global: {
@@ -39,7 +83,7 @@ describe('Test Rooms Tab', () => {
           }
         }
       })
-      wrapper.vm.$data.is_loading = false
+      await flushPromises()
       await expect(wrapper.findAll('td').filter(i => i.element.innerHTML.includes('Room1')).length).toBe(1)
     })
   })
