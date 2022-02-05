691d018648dfe3d59429c2bdcf77d7bfd8643d8d
Zhi Wang <ubwsx@student.kit.edu>
2022-02-05 14:11:46 +0100

    remove marking unused variable & bring back mock 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,9 +31,6 @@ module.exports = {
       ],
       env: {
         jest: true
-      },
-      rules: {
-        "no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }]
       }
     }
   ]
