0c3292c995972318da2dd780c2275f19d7bb99eb
André Ackermann <uospa@student.kit.edu>
2022-02-15 09:56:57 +0100

    Added some tests for tx creation

--- tests/unit/store/tx_offline_create_tx.spec.ts
+++ tests/unit/store/tx_offline_create_tx.spec.ts
@@ -1,7 +1,8 @@
 import store from '@/store/tx'
 import { MatrixRoomID } from '@/models/id.model'
 import { GroupedTransaction, PendingApproval, TxGraph } from '@/models/transaction.model'
-import { room_01_user_info } from '../mocks/mocked_user'
+import { room_01_user_info, user_1, user_2, user_a, user_3 } from '../mocks/mocked_user'
+import { uuidgen } from '@/utils/utils'
 
 interface State {
   transactions: Record<MatrixRoomID, {
@@ -13,7 +14,7 @@ interface State {
 }
 
 describe('Test action_create_tx_for_room', () => {
-  const action = store.actions.action_create_tx_for_room
+  const action = store.actions.action_create_tx_for_room as (context: any, payload: any) => Promise<boolean>
   const room_id = 'aaa'
   let state: State = {
     transactions: {
@@ -34,7 +35,16 @@ describe('Test action_create_tx_for_room', () => {
       if (r === room_id) {
         return room_01_user_info
       }
+    },
+    'auth/user_id': () => {
+      return user_2.user_id
     }
+  }
+  const getters = {
+    get_grouped_transactions_for_room: store.getters.get_grouped_transactions_for_room(state, null, null, null),
+    get_pending_approvals_for_room: store.getters.get_pending_approvals_for_room(state, null, null, null),
+    get_existing_group_ids_for_room: store.getters.get_existing_group_ids_for_room(state, null, null, null),
+    get_existing_tx_ids_for_room: store.getters.get_existing_tx_ids_for_room(state, null, null, null)
   }
   beforeEach(() => {
     state = { // clear mocks
@@ -54,7 +64,133 @@ describe('Test action_create_tx_for_room', () => {
   })
   describe('Test against implementation errors', () => {
     it('Test UUID´s are filled', async () => {
-      // To be done
+      const fake_grouped_tx1: GroupedTransaction = {
+        from: user_1,
+        group_id: uuidgen(),
+        state: 'approved',
+        txs: [],
+        description: '',
+        participants: [],
+        timestamp: new Date(),
+        pending_approvals: []
+      }
+      await expect(action({
+        state,
+        commit: jest.fn(),
+        dispatch: jest.fn(),
+        getters: getters,
+        rootGetters: rootGetters
+      }, {
+        room_id: 'aaa',
+        tx: fake_grouped_tx1
+      })).rejects.toThrow(new Error('Implementation Error: do not fill the UUIDs!'))
+    })
+    it('Test state is not set to defined', async () => {
+      const fake_grouped_tx1: GroupedTransaction = {
+        from: user_1,
+        group_id: '',
+        state: 'approved',
+        txs: [],
+        description: '',
+        participants: [],
+        timestamp: new Date(),
+        pending_approvals: []
+      }
+      await expect(action({
+        state,
+        commit: jest.fn(),
+        dispatch: jest.fn(),
+        getters: getters,
+        rootGetters: rootGetters
+      }, {
+        room_id: 'aaa',
+        tx: fake_grouped_tx1
+      })).rejects.toThrow(new Error('Implementation Error: set state to defined!'))
+    })
+    it('Test there are no pending approvals', async () => {
+      const fake_grouped_tx1: GroupedTransaction = {
+        from: user_1,
+        group_id: '',
+        state: 'defined',
+        txs: [],
+        description: '',
+        participants: [],
+        timestamp: new Date(),
+        pending_approvals: [{
+          event_id: '',
+          type: 'create',
+          group_id: '',
+          approvals: {},
+          from: user_1,
+          description: '',
+          timestamp: new Date(),
+          txs: []
+        }]
+      }
+      await expect(action({
+        state,
+        commit: jest.fn(),
+        dispatch: jest.fn(),
+        getters: getters,
+        rootGetters: rootGetters
+      }, {
+        room_id: 'aaa',
+        tx: fake_grouped_tx1
+      })).rejects.toThrow(new Error('Implementation Error: no pending approvals, especially not itself!'))
+    })
+    it('Test user in tx is not in the room', async () => {
+      const fake_grouped_tx1: GroupedTransaction = {
+        from: user_1,
+        group_id: '',
+        state: 'defined',
+        txs: [{
+          to: user_a,
+          tx_id: '',
+          amount: 5
+        }],
+        description: '',
+        participants: [],
+        timestamp: new Date(),
+        pending_approvals: []
+      }
+      await expect(action({
+        state,
+        commit: jest.fn(),
+        dispatch: jest.fn(),
+        getters: getters,
+        rootGetters: rootGetters
+      }, {
+        room_id: 'aaa',
+        tx: fake_grouped_tx1
+      })).rejects.toThrow(new Error('Implementation Error: user not in room!'))
+    })
+  })
+  describe('Test other errors', () => {
+    it('Test current user not part of the transaction', async () => {
+      const fake_grouped_tx1: GroupedTransaction = {
+        from: user_1,
+        group_id: '',
+        state: 'defined',
+        txs: [{
+          to: user_3,
+          tx_id: '',
+          amount: 5
+        }],
+        description: '',
+        participants: [],
+        timestamp: new Date(),
+        pending_approvals: []
+      }
+      await expect(action({
+        state,
+        commit: jest.fn(),
+        dispatch: jest.fn(),
+        getters: getters,
+        rootGetters: rootGetters
+      }, {
+        room_id: 'aaa',
+        tx: fake_grouped_tx1
+      })).rejects.toThrow(new Error('Error: current user not part of the transaction!'))
     })
   })
 })
