cad9b74f63314911234f35335191e893718fae5a
Zhi Wang <ubwsx@student.kit.edu>
2022-02-21 13:24:12 +0100

    scroll test added but not done

--- tests/e2e/specs/integration/store_transaction_information.spec.ts
+++ tests/e2e/specs/integration/store_transaction_information.spec.ts
@@ -5,7 +5,7 @@ describe('Test store_transaction_message', function () {
   beforeEach(function () {
     cy.login(1)
     cy.visit('room/!HJKUpUGgHFoSoqWlCj:dsn.tm.kit.edu')
-    cy.wait(5000)
+    cy.get('.alert-primary').should('not.exist', { timeout: 6000 })
   })
   it('Check if approved message exists', function () {
     const random_tx_name = uuidgen().substring(0, 7)
