37fed87f88973ad9e2b5dccc71e2cf6edf4dc30b
Xuyang <karl250@DESKTOP-DEJAB43.localdomain>
2022-02-22 16:38:03 +0100

    add rejoin test

---
+++ tests/e2e/specs/integration/join_leave_payment_group_1.spec.ts
@@ -0,0 +1,42 @@
+import { selectorify, uuidgen } from '@/utils/utils'
+import { user_1, user_2 } from '../../../unit/mocks/mocked_user'
+
+describe('Test join_leave_payment_group', function () {
+  it('Test kick a member(with openbalance)', function () {
+    cy.login(2)
+    cy.visit('room/!XfSkhdwHMNwuyctFJP:dsn.tm.kit.edu')
+    cy.wait(5000)
+    cy.get('#usercard_' + selectorify(user_2.user_id)).get('#leaveButton').click().then(() => {
+      cy.wait(1000)
+      cy.get('[data-cy=Yes]').click().then(() => {
+        cy.logout()
+        cy.wait(1000)
+        cy.login(1)
+        cy.visit('room/!XfSkhdwHMNwuyctFJP:dsn.tm.kit.edu')
+        cy.wait(5000)
+        cy.get('#inviteButton').click().then(() => {
+          cy.wait(1000)
+          cy.get('#invite-userid').type('@test-2:dsn.tm.kit.edu')
+          cy.wait(1000)
+          cy.get('#invite-confirm').click()
+          cy.wait(1000)
+          cy.logout()
+          cy.wait(1000)
+          cy.login(2)
+          cy.wait(1000)
+          cy.visit('/rooms')
+          cy.wait(5000)
+          cy.get('[data-cy=accept-invitation]').click().then(() => {
+            cy.wait(1000)
+            cy.get('.mb-5').children().last().find('.card').find('.card-body').find('.row').children().eq(1)
+              .find('p').contains('test-rejoin').should('be.visible')
+          }
+          )
+        })
+      }
+      )
+    }
+    )
+  }
+  )
+})
