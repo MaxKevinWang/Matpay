b2bab7335b747f691365710cfce2c8c8e0d44313
Yanxiu Wuwang <uofvo@student.kit.edu>
2022-02-23 10:47:10 +0100

    Add hint for logging in

--- src/tabs/Login.vue
+++ src/tabs/Login.vue
@@ -20,7 +20,15 @@
         <input v-model="password" id="password" class="form-control" type="password" name="password"
                placeholder="Password" @keyup.enter="login">
       </div>
-      <input class="btn btn-primary" @click="login" value="Login" id="login">
+      <button type="button" class="btn btn-primary" @click="login" id="login">
+        Login
+        <span
+          v-if="spin"
+          class="spinner-login spinner-border spinner-border-sm"
+          role="status"
+          aria-hidden="true">
+        </span>
+      </button>
     </form>
     Don't have an account? <router-link to="register">Register here.</router-link>
   </div>
@@ -36,7 +44,8 @@ export default defineComponent({
       username: '' as string,
       homeserver: this.$store.getters['auth/homeserver'] || 'https://tchncs.de' as string,
       password: '' as string,
-      error: '' as string
+      error: '' as string,
+      spin: false as boolean
     }
   },
   computed: {
@@ -57,15 +66,18 @@ export default defineComponent({
         return
       }
       try {
+        this.spin = true
         await this.action_login({
           username: this.username,
           homeserver: this.homeserver,
           password: this.password
         })
         await this.$router.push('/rooms')
+        this.spin = false
         this.error = ''
       } catch (reason: unknown) {
         this.error = (reason as Error).toString()
+        this.spin = false
       }
     }
   },
