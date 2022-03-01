fc61a2ad5751f6a1318f2b29d6a3c8488ff714d0
Yanxiu Wuwang <uofvo@student.kit.edu>
2022-03-01 09:40:14 +0100

    remove logout hook for tx approval, rename test

--- tests/e2e/specs/integration/tx_approval.spec.ts
+++ tests/e2e/specs/integration/tx_approval.spec.ts
@@ -1,9 +1,8 @@
 import { user_1, user_2 } from '../../../unit/mocks/mocked_user'
 import { selectorify, split_percentage, sum_amount, to_currency_display, uuidgen } from '@/utils/utils'
 
-describe('Test recall_tx_history', function () {
+describe('Test transaction approval', function () {
   afterEach(() => {
-    cy.logout()
     cy.wait(3000)
   })
   beforeEach(function () {
