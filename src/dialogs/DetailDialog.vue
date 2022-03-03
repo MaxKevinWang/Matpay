e105b9c48d81de5d791bc638b1b805f69e9462fc
Yanxiu Wuwang <uofvo@student.kit.edu>
2022-03-03 18:34:30 +0100

    bug fix: dialogs get mixed up when having multiple modifications

--- src/dialogs/DetailDialog.vue
+++ src/dialogs/DetailDialog.vue
@@ -3,7 +3,7 @@
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" :id="'DetailDialog-modal_' + this.reference.grouped_tx.group_id">Details</h5>
+          <h5 class="modal-title">Details</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" @click="hide()"></button>
         </div>
         <div class="modal-body">
